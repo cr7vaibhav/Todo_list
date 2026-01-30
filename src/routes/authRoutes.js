c4717e9dc8ceee590ad53aa54638c652244bbb46
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2026-01-30 11:40:14 +0530

    final

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,37 +7,57 @@ const router = express.Router()
 
 // Resgister a new user /auth/register
 router.post('/register', (req, res) => {
-    const { username, password } = req.body
-    //save username and irreversibly encryted password
-    
-    //encrypt the password
-    const hashedPassword = bcrypt.hashSync(password, 8)
+const { username, password } = req.body
+  //save username and irreversibly encryted password
 
-    //save the new user and hashed pass to db
+  //encrypt the password
+const hashedPassword = bcrypt.hashSync(password, 8)
+
+    // save the new user and hashed password to the db
     try {
-        const insertUser = db.prepare(`INSERT INTO users (username,password)
-            VALUES(?,?)`)
+        const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`)
         const result = insertUser.run(username, hashedPassword)
-        
-        // now we have a user I want to add a first todo for them
+
+        // now that we have a user, I want to add their first todo for them
         const defaultTodo = `Hello :) Add your first todo!`
-        const insertTodo = db.prepare(`INSERT INTO todos(user_id ,task)
-            VALUES(?,?)`)
+        const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
         insertTodo.run(result.lastInsertRowid, defaultTodo)
-        
-        //create a token
-    } catch (error) {
-        console.log(error.message)
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
         res.sendStatus(503)
     }
-    
 })
 
 router.post('/login', (req, res) => {
-    //we get he email and look up to password assciated with email in th db
-    //but we get it back encrypted so we cannot compare it to the
-    //one the user just entered so we encrypt the password using the exact same algo
+  //we get he email and look up to password assciated with email in th db
+  //but we get it back encrypted so we cannot compare it to the
+  //one the user just entered so we encrypt the password using the exact same algo
 
+const { username, password } = req.body
+
+    try {
+        const getUser = db.prepare('SELECT * FROM users WHERE username = ?')
+        const user = getUser.get(username)
+
+        // if we cannot find a user associated with that username, return out from the function
+        if (!user) { return res.status(404).send({ message: "User not found" }) }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password)
+        // if the password does not match, return out of the function
+        if (!passwordIsValid) { return res.status(401).send({ message: "Invalid password" }) }
+        console.log(user)
+
+        // then we have a successful authentication
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' })
+        res.json({ token })
+    } catch (err) {
+        console.log(err.message)
+        res.sendStatus(503)
+    }
 })
 
 export default router
--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -6,22 +6,41 @@ const router = express.Router()
 
 //get all todos for logged-in user
 router.get('/', (req, res) => {
-    
+    const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?')
+    const todos = getTodos.all(req.userId)
+    res.json(todos)
 })
 
-//create a new todo 
+// Create a new todo
 router.post('/', (req, res) => {
-    
+    const { task } = req.body 
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
+
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
 })
 
-//update a todo
+// Update a todo
 router.put('/:id', (req, res) => {
-    
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
+
+    const updatedTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+    updatedTodo.run(completed, id)
+
+    res.json({ message: "Todo completed" })
 })
 
-//delete a todo 
+// Delete a todo
 router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
     
+    res.send({ message: "Todo deleted" })
 })
+
 
 export default router
